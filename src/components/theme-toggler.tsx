6a9d5ba4094cd890b4c89ce5c11d8b935462d511
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-15 21:44:31 +0530

    Refactor theme toggler component to improve accessibility and styling

--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -5,10 +5,12 @@ import { useTheme } from "next-themes";
 export const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
   return (
-    <div className="flex items-center justify-center">
-      {theme}
-      <MoonStar className="w-6 h-6" onClick={() => setTheme("dark")} />
-      <SunIcon className="w-6 h-6" onClick={() => setTheme("light")} />
+    <div className="flex items-center justify-center border rounded-lg hover:bg-slate-500/30">
+      {theme === "dark" ? (
+        <SunIcon className="w-6 h-6 cursor-pointer p-1" onClick={() => setTheme("light")} />
+      ) : (
+        <MoonStar className="w-6 h-6 cursor-pointer p-1" onClick={() => setTheme("dark")} />
+      )}
     </div>
   );
 };

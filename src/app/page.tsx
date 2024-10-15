c01b281fc900497e097c71f5aa9edba7fa08dc9d
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-15 22:23:26 +0530

    Refactor page component and update Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Hero } from "@/components/hero";
-import { ThemeToggler } from "@/components/theme-toggler";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col items-center justify-center container max-w-screen min-h-screen font-[family-name:var(--font-geist-sans)] overflow-hidden">
+    <div className="relative flex flex-col items-center justify-center max-w-screen min-h-screen font-[family-name:var(--font-geist-sans)] overflow-hidden">
       <Hero />
+
     </div>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,12 +3,12 @@ import { Button } from "./ui/button";
 type Props = {};
 export const Hero = ({}: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 z-10 backdrop-blur-lg">
-      <h1 className="text-3xl font-bold">Welcome to Safe-crypt Sol</h1>
+    <div className="flex flex-col text-center gap-4">
+      <h1 className="sm:text-4xl text-2xl font-bold">Welcome to Safe-crypt Sol</h1>
       <p className="text-slate-500 text-lg">Let&apos;s get started</p>
       <div className="flex flex-col items-center justify-center mt-10 gap-4">
-        <Button>Get Started With A New Wallet</Button>
-        <Button variant={"outline"}>Import Wallet</Button>
+        <Button className="text-md">Get Started With A New Wallet</Button>
+        <Button className="text-md" variant={"outline"}>Import Wallet</Button>
       </div>
     </div>
   );

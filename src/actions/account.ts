d8cd101ad7a02a6482cfe2fa3226dc955ef17302
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-19 21:04:49 +0530

    Refactor code and dependencies for wallet creation

--- src/actions/account.ts
+++ src/actions/account.ts
@@ -2,20 +2,34 @@
 import { generateMnemonic } from "bip39";
 import { auth } from "@/lib/auth";
 import db from "@/lib/db";
-
-export const SaveMnemonics = async () => {
+import { Keypair } from "@solana/web3.js";
+export const createWallet = async () => {
   const sessionData = await auth();
   const user = sessionData?.user;
-  const mnemonics = generateMnemonic(128);
   try {
-    await db.user.update({
-      where: { id: user?.id },
+    if (!user) {
+      return { error: "User not found" };
+    }
+    if (!user.id) {
+      return { error: "User ID not found" };
+    }
+    const keypair = Keypair.generate();
+
+    const pubKey = keypair.publicKey.toString();
+    const privKey = keypair.secretKey.toString();
+
+    const wallet = await db.wallet.create({
       data: {
-        mnemonics: mnemonics.split(' '),
+        address: "",
+        name: (user.email?.split("@")[0] as string) + "'s Wallet",
+        privateKey: pubKey,
+        publicKey: privKey,
+        userId: user.id as string,
+        mnemonics: generateMnemonic(128).split(" "),
       },
     });
+    return wallet;
   } catch (error) {
     return error;
   }
 };
-
--- src/components/create-wallet.tsx
+++ src/components/create-wallet.tsx
@@ -1,17 +1,10 @@
 "use client";
-import { MnemonicWarning } from "@/components/mnemonic-warning";
-import { Password } from "@/components/password";
-import { SecretPhrase } from "@/components/secret-phrase";
-import { useState } from "react";
 
 type Props = {};
-const CreateWallet = ({}: Props) => {
-  const [tab, setTab] = useState("onboarding");
+const CreateWallet = ({ }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center max-w-screen min-h-screen gap-6">
-      {tab === "onboarding" && <MnemonicWarning setTab={setTab} />}
-      {tab === "secret" && <SecretPhrase setTab={setTab} />}
-      {tab === "wallet-created" && <Password />}
+      create wallet
     </div>
   );
 };

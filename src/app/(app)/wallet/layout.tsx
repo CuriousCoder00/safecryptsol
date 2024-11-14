d1753a0b98104a38fd5219475c8e30868ddcf230
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-14 09:51:39 +0530

    Refactor wallet layout and sidebar components to improve structure and add scroll functionality

--- src/app/(app)/wallet/layout.tsx
+++ src/app/(app)/wallet/layout.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Sidebar, SidebarItem } from "@/components/wallet/sidebar";
 
 export default function WalletLayout({
@@ -7,15 +8,11 @@ export default function WalletLayout({
 }>) {
   return (
     <div className="flex w-full h-full max-w-screen-2xl overflow-hidden min-h-screen px-2">
-      <div className="flex items-center justify-center w-full gap-3">
-        <Sidebar>
-          <SidebarItem>
-            <div className="rounded-full w-12 h-12 bg-slate-700 flex items-center justify-center">A1</div>
-          </SidebarItem>
-        </Sidebar>
-        <div className="flex flex-col h-full w-full max-h-[calc(100vh-72px)] mt-[72px] pb-2">
-          <div className="flex items-center justify-center  px-2 border rounded-xl h-full w-full shadow-inner shadow-slate-600">
-            {children}
+      <div className="flex items-center justify-center w-full gap-3 max-h-[calc(100vh-72px)] mt-[72px] pb-2">
+        <Sidebar/>
+        <div className="flex flex-col h-full w-full">
+          <div className="flex items-center justify-center border rounded-xl h-full w-full shadow-inner shadow-slate-600">
+            <ScrollArea className="h-full p-2">{children}</ScrollArea>
           </div>
         </div>
       </div>
--- src/components/wallet/sidebar.tsx
+++ src/components/wallet/sidebar.tsx
@@ -1,13 +1,40 @@
 import React from "react";
+import { ScrollArea } from "../ui/scroll-area";
 
-export const Sidebar = ({ children }: { children: React.ReactNode }) => {
+export const Sidebar = () => {
   return (
-    <div className="flex flex-col pt-[72px] pb-2 h-full text-white">
-      {children}
+    <div className="flex flex-col h-full text-white">
+      <SidebarItem />
     </div>
   );
 };
 
-export const SidebarItem = ({ children }: { children: React.ReactNode }) => {
-  return <div className="p-2 border rounded-xl h-full w-full shadow-inner shadow-slate-600">{children}</div>;
+export const SidebarItem = () => {
+  return (
+    <div className="border rounded-xl h-full w-full shadow-inner shadow-slate-600 relative flex flex-col items-center justify-between py-2">
+      <ScrollArea className="h-full p-2 flex flex-col gap-5">
+        <div className="flex flex-col items-start justify-start gap-2 h-full">
+          <div className="flex items-center justify-center h-12 w-12 rounded-full border">
+            A1
+          </div>
+        </div>
+      </ScrollArea>
+      <div className="flex items-center justify-center h-12 w-12 rounded-full bg-slate-600 hover:bg-slate-800 transition-all duration-150 cursor-pointer aspect-square">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-8 w-8 text-white hover:scale-110 transition-transform duration-150"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </div>
+    </div>
+  );
 };

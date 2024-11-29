dd52ac6b59c1ffc341ba8abe7d28274c43bb8edf
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-29 18:12:36 +0530

    Improve wallet creation experience: update routing after wallet creation and enhance loading indicator with additional messaging.

--- src/components/wallet/create-wallet.tsx
+++ src/components/wallet/create-wallet.tsx
@@ -8,22 +8,23 @@ export const CreateWallet = () => {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   useEffect(() => {
-    console.log("Creating a wallet...");
     startTransition(() => {
       CreateNewWallet().then((res) => {
-        router.push(`/wallet/${res.account?.id}/${res.wallet?.id}`);
+        router.push(`/wallet`);
       });
     });
   }, []);
   return (
     <>
       {isPending && (
-        <div className="flex flex-col items-center justify-center mt-12 shadow-inner shadow-slate-700 rounded-xl border p-2 gap-3 px-4">
-          <div className="flex items-center justify-center gap-2">
-            <Loader2 className="animate-spin" />
-            Creating a wallet...
+        <div className="fixed inset-0 flex items-center justify-center w-full h-full z-[9999999]">
+          <div className="flex flex-col items-center justify-center mt-12 shadow-inner shadow-slate-700 rounded-xl border p-2 gap-3 px-4">
+            <div className="flex items-center justify-center gap-2">
+              <Loader2 className="animate-spin" />
+              Creating a wallet...
+            </div>
+            <div className="flex">Please wait while we create your wallet.</div>
           </div>
-          <div className="flex">Please wait while we create your wallet.</div>
         </div>
       )}
     </>
